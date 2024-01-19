5b47669da89d3b0e15da374aefa4d4d9c12f1543
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-19 01:25:08 +0100

    op

--- opcodes.c
+++ opcodes.c
@@ -9,7 +9,35 @@
 */
 void push(stack_t **stack, unsigned int line_number)
 {
+char *token;
+int value;
 
+token = strtok(NULL, " \t\n");
+if (!token || !isdigit(*token))
+{
+fprintf(stderr, "L%d: usage: push integer\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+value = atoi(token);
+
+stack_t *new_node = malloc(sizeof(stack_t));
+if (!new_node)
+{
+fprintf(stderr, "Error: malloc failed\n");
+exit(EXIT_FAILURE);
+}
+
+new_node->n = value;
+new_node->prev = NULL;
+new_node->next = *stack;
+
+if (*stack)
+{
+(*stack)->prev = new_node;
+}
+
+*stack = new_node;
 }
 
 /**
@@ -21,5 +49,11 @@ void push(stack_t **stack, unsigned int line_number)
 */
 void pall(stack_t **stack, unsigned int line_number)
 {
+stack_t *current = *stack;
 
+while (current)
+{
+printf("%d\n", current->n);
+current = current->next;
+}
 }
